e6c282eaed67aff25623a313cbb1278d2fe4da25
unknown <yang.allie@gmail.com>
2020-09-26 11:07:22 +0800

    Add Todo

---
+++ src/components/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input v-model="text" />
+    <button> @clickAdd</button>
+    {{ text }}
+    <ul>
+      <li v-for="item in list" :key="item">
+        {{ item }}
+      </li>
+    </ul>
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    text: "",
+    list: ["a", "b", "c"],
+  }),
+ methods: {
+    add() {
+      if(!this.text)
+      this.list.push(ghis.text);
+      this.text = "";
+      },
+   },
+};
+</script>
+<style scoped>
+</style>
+
